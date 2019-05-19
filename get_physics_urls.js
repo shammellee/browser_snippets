416796e69fe0f7b2ab9dc031ef0d86faa0d56b78
Shammel Lee <holler@shammellee.com>
2019-05-19 16:51:02 -0400

    Add "get_physics_urls" script

---
+++ get_physics_urls.js
@@ -0,0 +1,25 @@
+javascript:
+(
+  function()
+  {
+    var SELECTOR = '#moreData span.title a'
+        ,urls    = Array.prototype.slice.call(document.querySelectorAll(SELECTOR))
+        ;
+
+    for(var i=0;i<urls.length;i++)
+    {
+      urls[i]=urls[i].href;
+    }
+
+    var text_field=document.createElement('textarea');
+
+    document.body.appendChild(text_field);
+
+    text_field.value=urls.join('\n');
+
+    text_field.select();
+    document.execCommand('copy');
+    document.body.removeChild(text_field);
+  }()
+)
+
