a33f837622d3d91f95e03dd5ed640d8375519911
Shammel Lee <holler@shammellee.com>
2019-08-16 12:52:45 -0400

    update "get_youtube_urls" script

--- get_youtube_urls.js
+++ get_youtube_urls.js
@@ -2,24 +2,30 @@ javascript:
 (
   function()
   {
-    var SELECTOR = 'a.ytd-playlist-thumbnail'
-        ,urls    = Array.prototype.slice.call(document.querySelectorAll(SELECTOR))
-        ;
+    var urls = get_items('a.ytd-playlist-thumbnail');
+
+    if (!urls.length){urls = get_items('a.ytd-thumbnail')}
 
     for(var i=0;i<urls.length;i++)
     {
       urls[i]=urls[i].href;
     }
 
-    var text_field=document.createElement('textarea');
+    text_field=document.createElement('textarea');
 
     document.body.appendChild(text_field);
 
     text_field.value=urls.join('\n');
-
     text_field.select();
     document.execCommand('copy');
     document.body.removeChild(text_field);
+
+    function get_items(s)
+    {
+      return Array.prototype.slice.call(document.querySelectorAll(s));
+    }
   }()
 )
+
+javascript:(function(){var urls=get_items('a.ytd-playlist-thumbnail');if(!urls.length){urls=get_items('a.ytd-thumbnail')}for(var i=0;i<urls.length;i++){urls[i]=urls[i].href}text_field=document.createElement('textarea');document.body.appendChild(text_field);text_field.value=urls.join('\n');text_field.select();document.execCommand('copy');document.body.removeChild(text_field);function get_items(s){return Array.prototype.slice.call(document.querySelectorAll(s))}}())
 
