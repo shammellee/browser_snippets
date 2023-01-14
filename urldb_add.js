9b452cce3c14673dfb771d07bfa4646a84c50556
Shammel Lee <holler@shammellee.com>
2023-01-14 11:09:42 -0500

    Update "urldb add" + "urldb copy" scripts

--- urldb_add.js
+++ urldb_add.js
@@ -1,7 +1,23 @@
-javascript:((()=>{
-  const [ls_key, ls] = ['urls', localStorage];
-  const urls         = ls.getItem(ls_key) || '';
+javascript:((() =>
+{
+  const
+    ls          = localStorage
+    ,ns         = s => `urldb_${s}`
+    ,ls_key     = ns('urls')
+    ,urls       = {}
+    ,url_string = ls.getItem(ls_key)
+    ;
 
-  ls.setItem(ls_key, urls + location.href + ' ');
+  (
+    (url_string && ('string' === typeof url_string) && url_string.split(' '))
+    || []
+  )
+    .forEach(u => urls[u] = 0)
+    ;
+
+  urls[location.href] = 0;
+
+  ls.setItem(ns('url_count'), Object.keys(urls).length);
+  ls.setItem(ls_key, Object.keys(urls).join(' ').trim());
   void 0;
 })())
--- urldb_copy.js
+++ urldb_copy.js
@@ -1,11 +1,18 @@
-javascript:((()=>{
+javascript:((() =>
+{
   const
-    ls_key      = 'urls'
+    ls_key      = 'urldb_urls'
     ,ls         = localStorage
+    ,urls       = {}
     ,text_field = document.createElement('textarea')
     ;
 
-  text_field.value = (ls.getItem(ls_key).trim().split(' ')).join('\n');
+  (ls.getItem(ls_key) || '')
+    .trim()
+    .split(' ')
+    .forEach(url => urls[url] = 0);
+
+  text_field.value = Object.keys(urls).join('\n');
 
   document.body.appendChild(text_field);
   text_field.select();
