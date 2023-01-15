9ef1a230c84f9635d612d49ac882eb44c009bcb0
Shammel Lee <holler@shammellee.com>
2023-01-15 00:42:04 -0500

    Update "urldb clear" script

--- urldb_clear.js
+++ urldb_clear.js
@@ -1,6 +1,12 @@
 javascript:((()=>{
-  const [ls_key, ls] = ['urls', localStorage];
+  const
+    ns      = s => `urldb_${s}`
+    ,ls_key = ns('urls')
+    ,ls     = localStorage
+    ;
+
   ls.removeItem(ls_key);
+  ls.setItem(ns('url_count'), 0);
   void 0;
 })())
 
